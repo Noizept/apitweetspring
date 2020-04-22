90a29f5fd4ddc564f9ac5cfdf730b74ec641c852
Sergio Viula <sbnoize@gmail.com>
2020-04-22 04:38:17 +0100

    Disable security for now... need find out how to take that login page off....

--- src/main/java/com/sergio/restapi/Config/WebSecurityConfig.java
+++ src/main/java/com/sergio/restapi/Config/WebSecurityConfig.java
@@ -1,7 +1,10 @@
 package com.sergio.restapi.Config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,6 +13,15 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    protected void configure(AuthenticationManagerBuilder auth)  {
+        //...
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
